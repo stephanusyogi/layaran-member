b370722efe5c2d6afa231df6909d42588fb69ed0
stephanusyogi <stephanusyogi12@gmail.com>
2025-01-27 13:00:32 +0700

    first commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,29 @@
+
+<footer class="content-footer footer bg-footer-theme">
+    <div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
+        <div class="mb-2 mb-md-0">
+            ©
+            <script>
+                document.write(new Date().getFullYear());
+            </script>
+            , made with ❤️ by
+            <a href="javascript:void(0)" target="_blank" class="footer-link fw-bolder">Layaran Livechat</a>
+        </div>
+        <div>
+            <a href="javascript:void(0)" class="footer-link me-4" target="_blank">Dashboard</a>
+            <a href="javascript:void(0)" target="_blank" class="footer-link me-4">Events</a>
+            <a
+                href="javascript:void(0)"
+                target="_blank"
+                class="footer-link me-4"
+                >Billings</a
+            >
+            <a
+                href="javascript:void(0)"
+                target="_blank"
+                class="footer-link me-4"
+                >My Profile</a
+            >
+        </div>
+    </div>
+</footer>
